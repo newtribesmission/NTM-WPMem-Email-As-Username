db7d45931405e8cd74f48db8d938cde23d7f8ccc
Stevish <Stevish@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-06 17:03:06 +0000

    Set Plugin URI
    git-svn-id: https://plugins.svn.wordpress.org/email-as-username-for-wp-members/trunk@909099 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ntm-wpmem-email-as-username/ntm-wpmem-email-as-username.php
+++ ntm-wpmem-email-as-username/ntm-wpmem-email-as-username.php
@@ -3,6 +3,7 @@
 Plugin Name: NTM WP-Members Email to Username
 Description: Removes the need to create a username (if wp-members is in use). Changes or removes appropriate items from forms, and adds the email address as the username. If WP-Members is no longer in use, there are plenty of plugins that offer this capability for WP's native registration and login functions
 Author: New Tribes Mission (Stephen Narwold)
+Plugin URI: https://github.com/newtribesmission/NTM-WPMem-Email-As-Username
 Version: 1.0
 
     Copyright (C) 2014  New Tribes Mission
