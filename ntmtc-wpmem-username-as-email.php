c510c035f7a507be39ec82399e190a7d9bba4205
Stevish <Stevish@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-06 17:00:31 +0000

    fix licnse info ntmtc-wpmem-username-as-email.php
    Add and correct copyright, license and ownership info in ntmtc-wpmem-username-as-email.php
    
    git-svn-id: https://plugins.svn.wordpress.org/email-as-username-for-wp-members/trunk@909093 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ntmtc-wpmem-username-as-email.php
+++ ntmtc-wpmem-username-as-email.php
@@ -2,8 +2,24 @@
 /*
 Plugin Name: NTM WP-Members Email to Username
 Description: Removes the need to create a username (if wp-members is in use). Changes or removes appropriate items from forms, and adds the email address as the username. If WP-Members is no longer in use, there are plenty of plugins that offer this capability for WP's native registration and login functions
-Author: Stephen Narwold
+Author: New Tribes Mission (Stephen Narwold)
 Version: 1.1
+
+    Copyright (C) 2014  New Tribes Mission
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 /*****************************/
