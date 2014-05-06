78ef4186397d8a8148d7b7b92388c32399c3fc1c
Stevish <Stevish@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-06 16:59:41 +0000

    Initial Commit
    git-svn-id: https://plugins.svn.wordpress.org/email-as-username-for-wp-members/trunk@909091 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ntmtc-wpmem-username-as-email.php
@@ -0,0 +1,119 @@
+<?php
+/*
+Plugin Name: NTM WP-Members Email to Username
+Description: Removes the need to create a username (if wp-members is in use). Changes or removes appropriate items from forms, and adds the email address as the username. If WP-Members is no longer in use, there are plenty of plugins that offer this capability for WP's native registration and login functions
+Author: Stephen Narwold
+Version: 1.1
+*/
+
+/*****************************/
+/****       Login         ****/
+/*****************************/
+//No real crucial changes here, since it's all done at registration. This is just to remind them to use their email address to log in
+function ntmtc_wpmem_login_username_to_email($inputs) {
+	//change the name of the username field to "Email" on the login form
+	if ($inputs[0]['tag'] == 'log') {
+		$inputs[0]['name'] = 'Email (Use your mission email address if applicable)';
+	}
+	return $inputs;
+}
+//WP-Members Filter on the array of input fields for the login form
+add_filter('wpmem_inc_login_inputs', 'ntmtc_wpmem_login_username_to_email');
+
+
+/*****************************/
+/****  Password Reset     ****/
+/*****************************/
+function ntmtc_wpmem_pwd_reset_with_email_only($arr) {
+	//Allow a password reset to be initiated without a username
+	if (!isset($arr['user']) || $arr['user'] == '') {
+		//If no username is given, assume the username is the same as the email
+		$arr['user'] = $arr['email'];
+	}
+	return $arr;
+}
+//WP-Members Filter on the arguments from a password reset request
+add_filter('wpmem_pwdreset_args', 'ntmtc_wpmem_pwd_reset_with_email_only');
+
+function ntmtc_wpmem_pwd_reset_form_remove_user_field($inputs) {
+	//Removes Username field from the "Reset Password" form
+	if ($inputs[0]['tag'] == 'user') {
+		$inputs[0] = $inputs[1];
+		unset($inputs[1]);
+	}
+	return $inputs;
+}
+//WP-Members Filter on the array of input fields for the password reset form
+add_filter('wpmem_inc_resetpassword_inputs', 'ntmtc_wpmem_pwd_reset_form_remove_user_field');
+
+
+/*****************************/
+/**** Registration/Update ****/
+/*****************************/
+function ntmtc_e2u_wpmem_reg_form($rows, $toggle) {
+	//Edit the registration form
+	if (isset($rows['username'])) {
+		//Remove username field from the registration form
+		unset($rows['username']);
+	}
+	if (is_user_logged_in()) {
+		//If they're updating their info, disable the email field
+		$rows['user_email']['field'] = '<p class="noinput">' . $rows['user_email']['value'] . '</p>';
+	}
+	return $rows;
+}
+//WP-Members Filter on the array of input fields for the registration form
+add_filter('wpmem_register_form_rows', 'ntmtc_e2u_wpmem_reg_form');
+
+
+function ntmtc_fill_user_with_email($fields) { 
+	//Fill in the missing username field with the user's email, and protect email address from change
+	
+	if (is_user_logged_in()) {
+		//They're updating their user. Since username is unchangeable, email needs to be as well.
+		global $current_user;
+		get_currentuserinfo();
+		//Set their email address to the email address on file (unchanged)
+		$fields['user_email'] =  $current_user->user_email;
+	}
+	if ($fields['username'] == '' || !isset($fields['username'])) {
+		//If the username isn't set, set it to the email address
+		$fields['username'] = $fields['user_email'];
+	}
+	return $fields;
+}
+//WP-Members Filter for registration and update data before WP-Mem's validation
+add_filter( 'wpmem_pre_validate_form', 'ntmtc_fill_user_with_email' );
+
+
+/*****************************/
+/**** User Self-Delete    ****/
+/*****************************/
+//Allow subscribers to delete their own users (this would be the only way to change their email address)
+function ntmtc_remove_logged_in_user() { 
+	//First, make sure user is logged in and only a subscriber (protects admins from deleting themselves in testing)
+	if (is_user_logged_in() && !current_user_can('edit_posts') && $_POST['ntmtc-delete-account'] == 'DELETE MY ACCOUNT') {
+		require_once(ABSPATH.'wp-admin/includes/user.php' );
+		$current_user = wp_get_current_user();
+		wp_delete_user( $current_user->ID );
+		wp_redirect('/missionary-services',302);
+		//Once deleted, send them to the Missionary Services homepage
+	}
+}
+add_action('init', 'ntmtc_remove_logged_in_user');
+
+function ntmtc_block_admin_pages_for_subscribers() {
+	//Don't allow subscriber-level users (non-admins) to access the admin area or admin bar
+	if (!current_user_can('edit_posts')) {
+		//For anyone who can't edit posts:
+		//Hide the admin bar
+		show_admin_bar(false);
+		if (is_admin()) {
+			//If they're accessing an admin page, redirect to the profile page
+			wp_redirect("/missionary-services/profile",302);
+		}
+	}
+}
+add_action('init','ntmtc_block_admin_pages_for_subscribers',0);
+
+?>
