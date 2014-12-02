9165a71c167a0f083e1d89144f5324b51fa6c271
lloc <re@lloc.de>
2014-12-02 16:19:00 +0100

    WordPress Coding Standards

--- email-as-username-for-wp-members.php
+++ email-as-username-for-wp-members.php
@@ -42,149 +42,199 @@ $ntmeau_redirect_on_admin_denial = '/missionary-services/profile';
 function ntmeau_plugin_init() {
 	load_plugin_textdomain( 'ntmeau', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
+
 add_action( 'init', 'ntmeau_plugin_init' );
 
-/*****************************/
-/****       Login         ****/
-/*****************************/
-
-//If an email address is entered in the username box, then look up the matching username and authenticate as per normal, using that.
+/**
+ * Login
+ * If an email address is entered in the username box, then look up the matching username and authenticate as per normal, using that.
+ *
+ * @param WP_User $user
+ * @param string $username
+ * @param string $password
+ *
+ * @return WP_User|WP_Error
+ */
 function ntmeau_email_login_authenticate( $user, $username, $password ) {
-	if ( is_a( $user, 'WP_User' ) )
+	if ( is_a( $user, 'WP_User' ) ) {
 		return $user;
+	}
 
-	if ( !empty( $username ) ) {
+	if ( ! empty( $username ) ) {
 		$username = str_replace( '&', '&amp;', stripslashes( $username ) );
-		$user = get_user_by( 'email', $username );
-		if ( isset( $user, $user->user_login, $user->user_status ) && 0 == (int) $user->user_status )
+		$user     = get_user_by( 'email', $username );
+		if ( isset( $user, $user->user_login, $user->user_status ) && 0 == (int) $user->user_status ) {
 			$username = $user->user_login;
+		}
 	}
 
 	return wp_authenticate_username_password( null, $username, $password );
 }
+
 remove_filter( 'authenticate', 'wp_authenticate_username_password', 20, 3 );
 add_filter( 'authenticate', 'ntmeau_email_login_authenticate', 20, 3 );
 
-//This is just to remind them to use their email address to log in
-function ntmeau_wpmem_login_username_to_email($inputs) {
+/**
+ * Login Form Filter
+ *
+ * WP-Members Filter on the array of input fields for the login form
+ *
+ * @param array $inputs
+ *
+ * @return array
+ */
+function ntmeau_wpmem_login_username_to_email( $inputs ) {
 	//change the name of the username field to "Email" on the login form
-	if ($inputs[0]['tag'] == 'log') {
+	if ( $inputs[0]['tag'] == 'log' ) {
 		//$ntmeau_login_field_name is defined at the top of this file
 		$inputs[0]['name'] = __( 'Email (Use your mission email address if applicable)', 'ntmeau' );
 	}
+
 	return $inputs;
 }
-//WP-Members Filter on the array of input fields for the login form
-add_filter('wpmem_inc_login_inputs', 'ntmeau_wpmem_login_username_to_email');
+
+add_filter( 'wpmem_inc_login_inputs', 'ntmeau_wpmem_login_username_to_email' );
 
 
-/*****************************/
-/****  Password Reset     ****/
-/*****************************/
-function ntmeau_wpmem_pwd_reset_with_email_only($arr) {
+/**
+ * Password Reset
+ *
+ * WP-Members Filter on the arguments from a password reset request
+ *
+ * @param array $arr
+ *
+ * @return array
+ */
+function ntmeau_wpmem_pwd_reset_with_email_only( $arr ) {
 	//Allow a password reset to be initiated without a username
-	if (!isset($arr['user']) || $arr['user'] == '') {
+	if ( ! isset( $arr['user'] ) || $arr['user'] == '' ) {
 		//If no username is given, look up username by email
 		if ( is_object( $user = get_user_by( 'email', $arr['email'] ) ) ) {
 			//Set username to the correct user for the given email address
 			$arr['user'] = $user->user_login;
-			
+
 			//If that didn't work, try looking up by username in case they entered that
 		} elseif ( is_object( $user = get_user_by( 'login', $arr['email'] ) ) ) {
 			//Set email to the correct user for the given username, and get the username into the user variable
-			$arr['user'] = $user->user_login;
+			$arr['user']  = $user->user_login;
 			$arr['email'] = $user->user_email;
 		} else {
 			//Last ditch effort, but if we get here, the email doesn't exist and the pass reset will fail
 			$arr['user'] = $arr['email'];
 		}
 	}
+
 	return $arr;
 }
-//WP-Members Filter on the arguments from a password reset request
-add_filter('wpmem_pwdreset_args', 'ntmeau_wpmem_pwd_reset_with_email_only');
 
-function ntmeau_wpmem_pwd_reset_form_remove_user_field($inputs) {
-	//Removes Username field from the "Reset Password" form
-	if ($inputs[0]['tag'] == 'user') {
+add_filter( 'wpmem_pwdreset_args', 'ntmeau_wpmem_pwd_reset_with_email_only' );
+
+/**
+ * Removes Username field from the "Reset Password" form
+ *
+ * WP-Members Filter on the array of input fields for the password reset form
+ *
+ * @param array $inputs
+ *
+ * @return array
+ */
+function ntmeau_wpmem_pwd_reset_form_remove_user_field( $inputs ) {
+	//
+	if ( $inputs[0]['tag'] == 'user' ) {
 		$inputs[0] = $inputs[1];
-		unset($inputs[1]);
+		unset( $inputs[1] );
 	}
+
 	return $inputs;
 }
-//WP-Members Filter on the array of input fields for the password reset form
-add_filter('wpmem_inc_resetpassword_inputs', 'ntmeau_wpmem_pwd_reset_form_remove_user_field');
+
+add_filter( 'wpmem_inc_resetpassword_inputs', 'ntmeau_wpmem_pwd_reset_form_remove_user_field' );
 
 
-/*****************************/
-/**** Registration/Update ****/
-/*****************************/
-function ntmeau_e2u_wpmem_reg_form($rows, $toggle = false) {
-	//Edit the registration form
-	if (isset($rows['username'])) {
+/**
+ * Edit the registration form
+ *
+ * WP-Members Filter on the array of input fields for the registration form
+ *
+ * @param array $rows
+ *
+ * @return array
+ */
+function ntmeau_e2u_wpmem_reg_form( $rows ) {
+	if ( isset( $rows['username'] ) ) {
 		//Remove username field from the registration form
-		unset($rows['username']);
+		unset( $rows['username'] );
 	}
-	if (is_user_logged_in()) {
+	if ( is_user_logged_in() ) {
 		//If they're updating their info, disable the email field
 		$rows['user_email']['field'] = '<p class="noinput">' . $rows['user_email']['value'] . '</p>';
 	}
+
 	return $rows;
 }
-//WP-Members Filter on the array of input fields for the registration form
-add_filter('wpmem_register_form_rows', 'ntmeau_e2u_wpmem_reg_form');
 
+add_filter( 'wpmem_register_form_rows', 'ntmeau_e2u_wpmem_reg_form' );
 
-function ntmeau_fill_user_with_email($fields) { 
-	//Fill in the missing username field with the user's email, and protect email address from change
-	
-	if (is_user_logged_in()) {
+/**
+ * Fill in the missing username field with the user's email, and protect email address from change
+ *
+ * WP-Members Filter for registration and update data before WP-Mem's validation
+ *
+ * @param array $fields
+ *
+ * @return array
+ */
+function ntmeau_fill_user_with_email( $fields ) {
+	if ( is_user_logged_in() ) {
 		//They're updating their user. Since username is unchangeable, email needs to be as well.
 		global $current_user;
 		get_currentuserinfo();
 		//Set their email address to the email address on file (unchanged)
-		$fields['user_email'] =  $current_user->user_email;
+		$fields['user_email'] = $current_user->user_email;
 	}
-	if ($fields['username'] == '' || !isset($fields['username'])) {
+	if ( $fields['username'] == '' || ! isset( $fields['username'] ) ) {
 		//If the username isn't set, set it to the email address
 		$fields['username'] = $fields['user_email'];
 	}
+
 	return $fields;
 }
-//WP-Members Filter for registration and update data before WP-Mem's validation
+
 add_filter( 'wpmem_pre_validate_form', 'ntmeau_fill_user_with_email' );
 
-
-/*****************************/
-/**** User Self-Delete    ****/
-/*****************************/
-//Allow subscribers to delete their own users (this would be the only way to change their email address)
-function ntmeau_remove_logged_in_user() { 
-	//First, make sure user is logged in and only a subscriber (protects admins from deleting themselves in testing)
-	if (is_user_logged_in() && !current_user_can('edit_posts') && $_POST['ntmeau-delete-account'] == 'DELETE MY ACCOUNT') {
-		require_once(ABSPATH.'wp-admin/includes/user.php' );
+/**
+ * User Self-Delete
+ *
+ * Allow subscribers to delete their own users (this would be the only way to change their email address)
+ */
+function ntmeau_remove_logged_in_user() {
+	// First, make sure user is logged in and only a subscriber (protects admins from deleting themselves in testing)
+	if ( is_user_logged_in() && ! current_user_can( 'edit_posts' ) && $_POST['ntmeau-delete-account'] == 'DELETE MY ACCOUNT' ) {
+		require_once( ABSPATH . 'wp-admin/includes/user.php' );
 		$current_user = wp_get_current_user();
 		wp_delete_user( $current_user->ID );
-		//$ntmeau_redirect_on_delete is defined at the top of this file
-		wp_redirect($ntmeau_redirect_on_delete,302);
-		//Once deleted, send them to the Missionary Services homepage
+		// $ntmeau_redirect_on_delete is defined at the top of this file
+		wp_redirect( $ntmeau_redirect_on_delete, 302 );
+		// Once deleted, send them to the Missionary Services homepage
 	}
 }
-add_action('init', 'ntmeau_remove_logged_in_user');
 
+add_action( 'init', 'ntmeau_remove_logged_in_user' );
+
+/**
+ * Don't allow subscriber-level users (non-admins) to access the admin area or admin bar
+ */
 function ntmeau_block_admin_pages_for_subscribers() {
-	//Don't allow subscriber-level users (non-admins) to access the admin area or admin bar
-	if (!current_user_can('edit_posts')) {
-		//For anyone who can't edit posts:
-		//Hide the admin bar
-		show_admin_bar(false);
-		if (is_admin()) {
-			//If they're accessing an admin page, redirect to the profile page
-			//$ntmeau_redirect_on_admin_denial is defined at the top of this file
-			wp_redirect($ntmeau_redirect_on_admin_denial,302);
+	if ( ! current_user_can( 'edit_posts' ) ) {
+		// For anyone who can't edit posts:
+		// Hide the admin bar
+		show_admin_bar( false );
+		if ( is_admin() ) {
+			// If they're accessing an admin page, redirect to the profile page
+			// $ntmeau_redirect_on_admin_denial is defined at the top of this file
+			wp_redirect( $ntmeau_redirect_on_admin_denial, 302 );
 		}
 	}
 }
-add_action('init','ntmeau_block_admin_pages_for_subscribers',0);
 
-?>
+add_action( 'init', 'ntmeau_block_admin_pages_for_subscribers', 0 );
